7bbb7720ed6f90944b8cfff9e9548d3bb2bce2a9
Enzo Contieri <enzo.c.contieri@gmail.com>
2023-10-27 16:41:03 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { StyleSheet, Switch, Text, View } from 'react-native';
 
 export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 flex justify-center items-center dark:bg-neutral-900 space-y-6">
+      <StatusBar style={colorScheme=="dark"? "light": "dark"} />
+      <View className="flex-row justify-center items-center space-x-2">
+        <Text className="text-xl dark:text-white">Dark Mode</Text>
+        <Switch value={colorScheme=='dark'} onChange={toggleColorScheme} />
+      </View>
+      
+      <Text className="mx-4 text-justify" style={colorScheme=='dark'? styles.textWhite: styles.textBlack}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+      </Text>
+      
     </View>
   );
 }
@@ -17,4 +27,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textWhite: {
+    color: 'white'
+  },
+  textBlack: {
+    color: 'black'
+  }
 });
